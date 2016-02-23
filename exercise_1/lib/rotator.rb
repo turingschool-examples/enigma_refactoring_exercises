096b3dd1b5ec5c5fc1ec26bcc97ad7aa30464336
Horace Williams <horace.d.williams@gmail.com>
2016-02-23 11:21:34 -0700

    rotator also needs char map

--- exercise_1/lib/rotator.rb
+++ exercise_1/lib/rotator.rb
@@ -1,6 +1,7 @@
 class Rotator
-  def initialize(rotations) # e.g. [13, 25, 36, 50]
+  def initialize(rotations, character_map) # e.g. [13, 25, 36, 50]
     @rotations = rotations
+    @char_map = character_map
   end
 
   def rotate(character, index, direction = :forward)
