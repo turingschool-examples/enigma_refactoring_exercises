de685bf74bcc4ecce3658b23dd7798c8202c3671
Horace Williams <horace.d.williams@gmail.com>
2016-02-23 11:27:14 -0700

    remove some debug puts

--- exercise_1/lib/rotator.rb
+++ exercise_1/lib/rotator.rb
@@ -7,8 +7,6 @@ class Rotator
   def rotate(character, index, direction = :forward)
     start_index = @char_map.index(character)
     current_offset = @rotations[index]
-    puts "character: #{character.inspect}"
-    puts "char map: #{@char_map}"
     if direction == :forward
       @char_map.rotate(current_offset)[start_index]
     else
